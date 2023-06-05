2c4fadb0f977a2f9f56fcb4f14c604e5377a7e00
Emma Gerigscott <elgerig@gmail.com>
2023-06-04 17:11:19 -0700

    sample project with NYT API call and Mvc scaffolding

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcApiCall.Models;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace MvcApiCall.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly string _apikey;
+
+    public HomeController(IConfiguration configuration)
+    {
+        _apikey = configuration["NYT"];
+    }
+
+    public IActionResult Index()
+    {
+        List<Article> allArticles = Article.GetArticles(_apikey);
+        return View(allArticles);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
